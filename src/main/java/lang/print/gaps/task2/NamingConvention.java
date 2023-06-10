f5e7b0ab2d346467c2cc359a3cf959c9b3fe8b24
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-10 16:33:20 +0400

    git commit -m"Modify advancedNamingConvention.java" -m"Modify CopiedNames.java" -m"Modify JavaVariableNames.java " -m"Modify NamingConvention.java"

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        final int _variable = 10;
-        final int variable_One = 1;
-        final  int variableTwo = 2;
-        final  int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEfIVE = 5;
-        final  int VariableSix = 6;
-        final  int variableseven = 7;
-        final int VariableEight = 8;
+          int _variable = 10;
+          int variable_One = 1;
+          int variableTwo = 2;
+          int variable_three = 3;
+        final int variableFour = 4;
+        final int variableFive = 5;
+          int variableSix = 6;
+          int variableSeven = 7;
+          int variableEight = 8;
     }
 }
