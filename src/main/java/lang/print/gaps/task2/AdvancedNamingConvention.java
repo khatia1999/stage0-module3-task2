70700c80a036f628d19823609d459837e5e9606e
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-10 17:03:07 +0400

    git commit -m"Modify advancedNamingConvention.java" -m"Modify CopiedNames.java" -m"Modify JavaVariableNames.java " -m"Modify NamingConvention.java"

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -2,10 +2,8 @@ package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
 
-    private static final int ADULT_AGE = 18;
 
-    private int age;
-    private int phoneNumber;
+
 
     public void callToFriend() {
         //some code
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,10 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variableOne = 0;
-        int variableTwo = 2000;
-        int valueOne = 1000;
-        int variableThree = 4000;
-        int valueTwo = 3000;
+
+
         int variables = 5000;
-        variables = 7000;
-        int value3 = 90000;
+
+
     }
 }
