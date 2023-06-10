c85491fea6d453225d9df602a51cb447430baeb3
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-10 16:21:29 +0400

    git commit -m"Modify advancedNamingConvention.java" -m"Modify CopiedNames.java" -m"Modify JavaVariableNames.java " -m"Modify NamingConvention.java"

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,14 +2,14 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
-        int variable_three = 3;
+        final int _variable = 10;
+        final int variable_One = 1;
+        final  int variableTwo = 2;
+        final  int variable_three = 3;
         final int VARIABLEFOUR = 4;
         final int VARIABLEfIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        final  int VariableSix = 6;
+        final  int variableseven = 7;
+        final int VariableEight = 8;
     }
 }
--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,19 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
+
     private static final int adult_age = 18;
 
     private int AGE;
     private int phoneNumber;
 
     void CAllToFriend() {
-        //some code
+        System.out.println("my friend is :" + adult_age + "old ");
     }
 
     void callByNumber(int Number) {
-        //some code
+        advancedNamingConvention num = new advancedNamingConvention();
+        num.phoneNumber = 555555555;
+        System.out.println("my friend phoneNumber is :" + num );
     }
 }
